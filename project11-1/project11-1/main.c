6b3fae11f0471a8985baa458b7031e18f0748b73
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-11-13 15:32:57 +0900

    project11-3

--- project11-1/project11-1/main.c
+++ project11-1/project11-1/main.c
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void swap (int* x, int* y) //x랑 y를 포인터 변수로 변경
+{
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp; //등장하는 모든 x랑 y에 포인터 * 추가
+}
+
 int main(int argc, char * argv[])
 {
-    int i = 300;
+    int a = 3, b = 5;
     
-    int *pi = &i;
-    char *pc = &i;
+    swap(&a,&b); // 얘도 주소로 바꿔줌
     
-    printf("%i, %i, %i\n", i, *pi, *pc);
+    printf("a:%i, b:%i\n", a, b);
     
     system("PAUSE");
     return 0;
     
 }
 
-//300 넣으면 300,300,44
-//300을 넣었을 때 *pc에 44가 들어가는 이유는 300-44=256
-//0~255 표현가능
+//void swap (int x, int y) 실행하면 스왑 안되고 a,b 그대로 나옴 call by value 라서
+//int x int y 가 지역변수이기 때문
+//포인터 변수를 이용하면 스왑을 실행시킬수있게됨
