efba4d250979cebce4cdd29970821a6080ec6b2d
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-11-13 15:39:16 +0900

    project11-4

--- project11-1/project11-1/main.c
+++ project11-1/project11-1/main.c
@@ -2,27 +2,29 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void swap (int* x, int* y) //x랑 y를 포인터 변수로 변경
-{
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp; //등장하는 모든 x랑 y에 포인터 * 추가
-}
-
 int main(int argc, char * argv[])
 {
-    int a = 3, b = 5;
+    int i;
+    int grade[5];
+    int sum = 0;
     
-    swap(&a,&b); // 얘도 주소로 바꿔줌
+    for(i=0;i<5;i++)
+    {
+        printf("grade[%i] = ", i);
+        scanf("%d", &grade[i]);
+    }
     
-    printf("a:%i, b:%i\n", a, b);
+    for(i=0;i<5;i++)
+    {
+        printf("grade[%i] = %i\n", i, *(grade+i));
+        sum = sum + *(grade+i);
+
+    }
+    
+    printf("Average : %i\n", sum/5);
+    
     
     system("PAUSE");
     return 0;
     
 }
-
-//void swap (int x, int y) 실행하면 스왑 안되고 a,b 그대로 나옴 call by value 라서
-//int x int y 가 지역변수이기 때문
-//포인터 변수를 이용하면 스왑을 실행시킬수있게됨
